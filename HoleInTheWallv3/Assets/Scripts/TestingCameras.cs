c33e8502a3776be8c1431e268925224fdb901abf
Aidanjm13 <ajmathu13@outlook.com>
2024-07-12 22:13:29 -0400

    Can now view in scene if wanted

--- HoleInTheWallv3/Assets/Scripts/TestingCameras.cs
+++ HoleInTheWallv3/Assets/Scripts/TestingCameras.cs
@@ -14,8 +14,9 @@ public class TestingCameras : MonoBehaviour
 
     public Canvas canvas;
     public GameObject canvasFollow;
-    private float rotateSpeed = 50f;
     private float distance = 3f;
+
+    public bool showInXR = true;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +27,14 @@ public class TestingCameras : MonoBehaviour
         cameraImage.material.mainTexture = webcamTexture;
         webcamTexture.Play();
 
+        //camera for showing the avatars movements
         renderTexture = new RenderTexture(Screen.width, Screen.height, 16);
         avatarCamera.targetTexture = renderTexture;
         avatarImage.texture = renderTexture;
 
-        canvas.transform.position = canvasFollow.transform.position + new Vector3(0, 0, distance);
+        //moving the images to see the camera images
+        if (showInXR) canvas.transform.position = canvasFollow.transform.position + new Vector3(0, 0, distance);
+        else canvas.transform.position = new Vector3(14, 3, 3);
     }
 
     void OnDestroy()
@@ -41,14 +45,15 @@ public class TestingCameras : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 inFrontOfCamera = canvasFollow.transform.position + canvasFollow.transform.forward * distance;
-
-        // Update the object's position
-        canvas.transform.position = inFrontOfCamera;
-
-        // Optionally, make the object face the same direction as the camera
-        canvas.transform.rotation = canvasFollow.transform.rotation;
-
+        if (showInXR)
+        {
+            Vector3 inFrontOfCamera = canvasFollow.transform.position + canvasFollow.transform.forward * distance;
+            //Update the object's position
+            canvas.transform.position = inFrontOfCamera;
+            //make the object face the same direction as the camera
+            canvas.transform.rotation = canvasFollow.transform.rotation;
+        }
+        //move the camera that is facing the image
         avatarCamera.transform.position = canvasFollow.transform.position + new Vector3(0, 0, 3.05f);
     }
 }
